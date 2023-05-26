adbc1fa85e638257d89e80e87267b88e0f35bfc4
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-26 20:19:10 -0300

    entrada valida

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Entrada Valida");
+int numerodigitado = -1;
+
+while(numerodigitado <0 || numerodigitado >9)
+{
+    Console.WriteLine ("digite um número entre 1 e 9 (ou 0 para cancelar)...: ");
+    numerodigitado = Convert.ToInt32 (Console.ReadLine());
+}
+if (numerodigitado==0)
+{
+    Console.WriteLine("\noperação cancelada. ");
+}
+else
+{
+    Console.WriteLine($"\nnumeroselecionado = {numerodigitado}");
+}
+Console.WriteLine ("volte sempre! ");
+
